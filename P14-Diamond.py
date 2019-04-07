efe1aecfb6e5c5ef0bdd6db13fbf406172fda565
hsiehpanggi <teacher.hsieh@gmail.com>
2019-04-07 23:51:44 +0800

    p14

---
+++ P14-Diamond.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 21:45:13 2019
+
+@author: hsieh
+"""
+a=0
+n=4
+while 1:
+    if n==0 : break
+    n=int(input('pls input n > '))
+    star(n)
+    
+def star(n):
+    for i in range(n):
+        space=n-1-i
+        star=1+i*2
+        show=' '*space+'*'*star
+        print(show)
+    for j in range(n):
+        space +=1
+        star -=2
+        show=' '*space+'*'*star
+        print(show)
+    
+    
+    
+
