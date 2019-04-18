bbb4e40d6dd5e68ce3756dd7e933157015c806af
panggihsieh <7624220+panggihsieh@users.noreply.github.com>
2019-04-18 15:47:07 +0800

    from big to small pyramid

---
+++ P45_tree.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 11:49:12 2019
+
+@author: hsieh
+"""
+
+line=''
+row=5
+
+
+for i in range(row):
+    s1='-'*(row-1-i)
+    s2='/'
+    s3='*'*(2*i)
+    s4='\\'
+    line=s1+s2+s3+s4+s1
+    print(line)
+
+
+for i in range(row):
+    for j in range(3):
+        if i < j:
+            line='-'*(row*2-j*2)
+            print(line,end='')
+        else:
+            s1='-'*(row-1-i)
+            s2='/'
+            s3='*'*(2*i-2*j)
+            s4='\\'
+            line=s1+s2+s3+s4+s1
+            print(line,end='')
+    print()
+    line=''
